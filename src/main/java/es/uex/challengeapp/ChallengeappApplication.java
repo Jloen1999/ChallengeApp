49c88f1d1a2fd0570bb94040365714c589e8e64e
jloen1999 <jloelananguan@gmail.com>
2024-12-18 20:11:27 +0100

    Preparando Despliegue a Heroku

--- src/main/java/es/uex/challengeapp/ChallengeappApplication.java
+++ src/main/java/es/uex/challengeapp/ChallengeappApplication.java
@@ -2,11 +2,26 @@ package es.uex.challengeapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class ChallengeappApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ChallengeappApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ChallengeappApplication.class, args);
+    }
+
+    @Component
+    static
+    class ServerPortCustomizer implements WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> {
+        @Override
+        public void customize(ConfigurableServletWebServerFactory factory) {
+            String port = System.getenv("PORT");
+            if (port != null) {
+                factory.setPort(Integer.parseInt(port));
+            }
+        }
+    }
 }
